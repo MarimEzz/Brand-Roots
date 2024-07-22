51412bebab9b183cf4d8f2f5df37948b70233480
MarimEzz <marimezzaldin670@gmail.com>
2024-07-22 15:30:42 +0300

    first commit

---
+++ Arb/arbscript.js
@@ -0,0 +1,296 @@
+window.addEventListener('resize', home , service , about , contact);
+
+function home()
+{
+    if((window.outerWidth<=1223))
+    {
+        document.querySelector('.menu-body').style.display='none';
+    }
+    else if((window.outerWidth>=1024)&&(window.outerWidth<=1279))
+    {
+        document.querySelector('.font-nav-h').style.fontSize='37px';
+        document.querySelector('.font-nav-s').style.fontSize='30px';
+        document.querySelector('.font-nav-a').style.fontSize='30px';      
+        document.querySelector('.font-nav-c').style.fontSize='30px';                                                                    
+    }
+    else if((window.outerWidth>=1280)&&(window.outerWidth<=1365))
+    {
+        document.querySelector('.font-nav-h').style.fontSize='37px';
+        document.querySelector('.font-nav-s').style.fontSize='30px';
+        document.querySelector('.font-nav-a').style.fontSize='30px';      
+        document.querySelector('.font-nav-c').style.fontSize='30px';                                                                    
+    }
+    else if((window.outerWidth>=1366)&&(window.outerWidth<=1439))
+
+    {
+        document.querySelector('.font-nav-h').style.fontSize='37px';
+        document.querySelector('.font-nav-s').style.fontSize='30px';
+        document.querySelector('.font-nav-a').style.fontSize='30px';      
+        document.querySelector('.font-nav-c').style.fontSize='30px';                                                                
+    }
+    else if((window.outerWidth>=1440)&&(window.outerWidth<=1919))
+    {
+        document.querySelector('.font-nav-h').style.fontSize='37px';
+        document.querySelector('.font-nav-s').style.fontSize='30px';
+        document.querySelector('.font-nav-a').style.fontSize='30px';      
+        document.querySelector('.font-nav-c').style.fontSize='30px';                                                                
+    }
+    else if((window.outerWidth>=1920))
+    {
+        document.querySelector('.font-nav-h').style.fontSize='37px';
+        document.querySelector('.font-nav-s').style.fontSize='30px';
+        document.querySelector('.font-nav-a').style.fontSize='30px';      
+        document.querySelector('.font-nav-c').style.fontSize='30px';                                                                    
+    }
+}
+function service()
+{
+    if((window.outerWidth<=1223))
+    {
+        document.querySelector('.menu-body').style.display='none';
+    }
+    else if((window.outerWidth>=1024)&&(window.outerWidth<=1279))
+    {
+        document.querySelector('.font-nav-h').style.fontSize='30px';
+        document.querySelector('.font-nav-s').style.fontSize='37px';
+        document.querySelector('.font-nav-a').style.fontSize='30px'; 
+        document.querySelector('.font-nav-c').style.fontSize='30px';       
+    }
+    else if((window.outerWidth>=1280)&&(window.outerWidth<=1365))
+    {
+        document.querySelector('.font-nav-h').style.fontSize='30px';
+        document.querySelector('.font-nav-s').style.fontSize='37px';
+        document.querySelector('.font-nav-a').style.fontSize='30px'; 
+        document.querySelector('.font-nav-c').style.fontSize='30px';       
+    }
+    else if((window.outerWidth>=1366)&&(window.outerWidth<=1439))
+    {
+        document.querySelector('.font-nav-h').style.fontSize='30px';
+        document.querySelector('.font-nav-s').style.fontSize='37px';
+        document.querySelector('.font-nav-a').style.fontSize='30px'; 
+        document.querySelector('.font-nav-c').style.fontSize='30px';                                                                
+    }
+    else if((window.outerWidth>=1440)&&(window.outerWidth<=1919))
+    {
+        document.querySelector('.font-nav-h').style.fontSize='30px';
+        document.querySelector('.font-nav-s').style.fontSize='37px';
+        document.querySelector('.font-nav-a').style.fontSize='30px'; 
+        document.querySelector('.font-nav-c').style.fontSize='30px';   
+    }
+    else if((window.outerWidth>=1920))
+    {
+        document.querySelector('.font-nav-h').style.fontSize='30px';
+        document.querySelector('.font-nav-s').style.fontSize='37px';
+        document.querySelector('.font-nav-a').style.fontSize='30px'; 
+        document.querySelector('.font-nav-c').style.fontSize='30px';       
+    }
+
+}
+function about()
+{
+    if((window.outerWidth<=1223))
+    {
+        document.querySelector('.menu-body').style.display='none';
+    }
+    else if((window.outerWidth>=1024)&&(window.outerWidth<=1279))
+    {
+        document.querySelector('.font-nav-h').style.fontSize='30px';
+        document.querySelector('.font-nav-s').style.fontSize='30px';
+        document.querySelector('.font-nav-a').style.fontSize='37px';     
+        document.querySelector('.font-nav-c').style.fontSize='30px';       
+    }
+    else if((window.outerWidth>=1280)&&(window.outerWidth<=1365))
+    {
+        document.querySelector('.font-nav-h').style.fontSize='30px';
+        document.querySelector('.font-nav-s').style.fontSize='30px';
+        document.querySelector('.font-nav-a').style.fontSize='37px';     
+        document.querySelector('.font-nav-c').style.fontSize='30px';       
+    }
+    else if((window.outerWidth>=1366)&&(window.outerWidth<=1439))
+    {
+        document.querySelector('.font-nav-h').style.fontSize='30px';
+        document.querySelector('.font-nav-s').style.fontSize='30px';
+        document.querySelector('.font-nav-a').style.fontSize='37px';     
+        document.querySelector('.font-nav-c').style.fontSize='30px';    
+    }
+    else if((window.outerWidth>=1440)&&(window.outerWidth<=1919))
+    {
+        document.querySelector('.font-nav-h').style.fontSize='30px';
+        document.querySelector('.font-nav-s').style.fontSize='30px';
+        document.querySelector('.font-nav-a').style.fontSize='37px';     
+        document.querySelector('.font-nav-c').style.fontSize='30px';   
+    }
+    else if((window.outerWidth>=1920))
+    {
+        document.querySelector('.font-nav-h').style.fontSize='30px';
+        document.querySelector('.font-nav-s').style.fontSize='30px';
+        document.querySelector('.font-nav-a').style.fontSize='37px';     
+        document.querySelector('.font-nav-c').style.fontSize='30px';           }
+}
+function contact()
+{
+    if((window.outerWidth<=1223))
+    {
+        document.querySelector('.menu-body').style.display='none';
+    }
+    else if((window.outerWidth>=1024)&&(window.outerWidth<=1279))
+    {
+        document.querySelector('.font-nav-h').style.fontSize='30px';
+        document.querySelector('.font-nav-s').style.fontSize='30px';
+        document.querySelector('.font-nav-a').style.fontSize='30px';     
+        document.querySelector('.font-nav-c').style.fontSize='37px';    
+    }
+    else if((window.outerWidth>=1280)&&(window.outerWidth<=1365))
+    {
+        document.querySelector('.font-nav-h').style.fontSize='30px';
+        document.querySelector('.font-nav-s').style.fontSize='30px';
+        document.querySelector('.font-nav-a').style.fontSize='30px';     
+        document.querySelector('.font-nav-c').style.fontSize='37px';    
+    }
+    else if((window.outerWidth>=1366)&&(window.outerWidth<=1439))
+    {
+        document.querySelector('.font-nav-h').style.fontSize='30px';
+        document.querySelector('.font-nav-s').style.fontSize='30px';
+        document.querySelector('.font-nav-a').style.fontSize='30px';     
+        document.querySelector('.font-nav-c').style.fontSize='37px';    
+    }
+    else if((window.outerWidth>=1440)&&(window.outerWidth<=1919))
+    {
+        document.querySelector('.font-nav-h').style.fontSize='30px';
+        document.querySelector('.font-nav-s').style.fontSize='30px';
+        document.querySelector('.font-nav-a').style.fontSize='30px';     
+        document.querySelector('.font-nav-c').style.fontSize='37px';
+    }
+    else if((window.outerWidth>=1920))
+    {
+        document.querySelector('.font-nav-h').style.fontSize='30px';
+        document.querySelector('.font-nav-s').style.fontSize='30px';
+        document.querySelector('.font-nav-a').style.fontSize='30px';     
+        document.querySelector('.font-nav-c').style.fontSize='37px';        }
+}
+    
+function menu ()
+{
+    var x = document.querySelector('.menu-body');
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+
+
+function ranacontact()
+{
+    document.getElementById('contact-rana-tag').style.width='220px';    
+    document.getElementById('contact-rana-tag').style.left='70px';   
+    document.getElementById('contact-rana-tag').style.visibility='visible';    
+}
+/* for button on mobile NOT WORKING
+function ranacontactp()
+{
+    document.getElementById('contact-rana-tag-p').style.width='220px';    
+    document.getElementById('contact-rana-tag-p').style.left='70px';   
+    document.getElementById('contact-rana-tag-p').style.visibility='visible';    
+}
+*/
+/*var ranabtn = document.getElementById('rana');
+    ranabtn.addEventListener('click', function() {
+    document.getElementById('contact-rana-tag-p').style.width='220px';    
+    document.getElementById('contact-rana-tag-p').style.left='35px';  
+    document.getElementById('contact-rana-tag-p').style.visibility='visible'; });*/
+
+function manarcontact()
+{
+    document.getElementById('contact-manar-tag').style.width='220px';    
+    document.getElementById('contact-manar-tag').style.left='70px';   
+    document.getElementById('contact-manar-tag').style.visibility='visible';    
+}
+function saracontact()
+{
+    document.getElementById('contact-sara-tag').style.width='220px';    
+    document.getElementById('contact-sara-tag').style.left='70px';   
+    document.getElementById('contact-sara-tag').style.visibility='visible';    
+}
+function samarcontact()
+{
+    document.getElementById('contact-samar-tag').style.width='220px';    
+    document.getElementById('contact-samar-tag').style.left='70px';   
+    document.getElementById('contact-samar-tag').style.visibility='visible';    
+}
+function amgadcontact()
+{
+    document.getElementById('contact-amgad-tag').style.width='220px';    
+    document.getElementById('contact-amgad-tag').style.left='70px';   
+    document.getElementById('contact-amgad-tag').style.visibility='visible';    
+}
+function marwacontact()
+{
+    document.getElementById('contact-marwa-tag').style.width='220px';    
+    document.getElementById('contact-marwa-tag').style.left='70px';   
+    document.getElementById('contact-marwa-tag').style.visibility='visible';    
+}
+function nourcontact()
+{
+    document.getElementById('contact-nourhan-tag').style.width='220px';    
+    document.getElementById('contact-nourhan-tag').style.left='70px';   
+    document.getElementById('contact-nourhan-tag').style.visibility='visible';    
+}
+
+function animation()
+{
+        const counters = document.querySelectorAll('.value');
+        const speed = 350;
+        
+        counters.forEach( counter => {
+           const animate = () => {
+              const value = +counter.getAttribute('val');
+              const data = +counter.innerText;
+             
+              const time = value / speed;
+             if(data < value) {
+                  counter.innerText = Math.ceil(data + time);
+                  setTimeout(animate, 1);
+                }else{
+                  counter.innerText ='+' + value;
+                }
+             
+           }
+           
+           animate();
+        });
+}
+
+window.addEventListener('scroll' , ( ) =>{
+    const homescroll = document.getElementById('home').getBoundingClientRect().top +  window.scrollY;
+    const servicescroll = document.getElementById('service').getBoundingClientRect().top +  window.scrollY;
+    const aboutscroll = document.getElementById('about').getBoundingClientRect().top +  window.scrollY;
+    const contactscroll = document.getElementById('contact').getBoundingClientRect().top +  window.scrollY;
+    const numanimation = document.getElementById('num-animate').getBoundingClientRect().top +  window.scrollY;
+    const scrolled = window.scrollY ;
+    //console.log(servicescroll)  ; 
+    //console.log(scrolled)  ; 
+
+    if ( Math.ceil ( scrolled ) >= homescroll ) 
+    {
+        home();
+    }
+    if ( Math.ceil ( scrolled ) >= servicescroll ) 
+    {
+        service();
+    }
+    if (Math.ceil ( scrolled ) >= aboutscroll)
+    {
+        about();
+    }
+    if (Math.ceil ( scrolled ) >= contactscroll)
+    {
+        contact();
+    }
+    if (Math.ceil ( scrolled ) >= numanimation)
+    {
+        animation();
+    }
+    
+});
